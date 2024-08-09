305c95c41387a41fd67b8a923c5e46dc32918197
Egrosberg <ecgrosberg@gmail.com>
2024-08-09 15:01:20 -0400

    cognito changes

--- employee-accounts-web-app/src/aws/aws-exports.js
+++ employee-accounts-web-app/src/aws/aws-exports.js
@@ -3,8 +3,8 @@
 const awsmobile = {
     "aws_project_region": "us-east-1",
     "aws_cognito_region": "us-east-1",
-    "aws_user_pools_id": "myUserPoolId",
-    "aws_user_pools_web_client_id": "myWebClientId"
+    "aws_user_pools_id": "us-east-1_IVjpfWrA5",
+    "aws_user_pools_web_client_id": "3k04g5mi1sftrucna2rfsd3nf4"
   };
   
 //   export default awsmobile;
